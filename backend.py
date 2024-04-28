fbfb8c9a054cd5dd61abed31d7ae6e1c812c0fce
ItsNeil <itsneil73@gmail.com>
2024-04-28 19:31:20 +0530

    implement get player on the API

--- backend.py
+++ backend.py
@@ -4,7 +4,7 @@ API_URL = "https://api.willofsteel.me"
 
 class Route:
     def __init__(self, path, method):
-        self.path = path
+        self.path = API_URL + path
         self.method = method.upper()
 
     def __str__(self):
@@ -20,5 +20,16 @@ class Route:
         return hash((self.path, self.method))
 
 class API:
-    def __init__(self):
-        pass
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.headers = {
+            "API-Key": api_key
+        }
+
+    def get_player(self):
+        route = Route("/player", "GET")
+        response = requests.get(route.path, headers=self.headers)
+        if response.status_code == 200:
+            return response.json()
+        # handle error here. ONLY error here is 403 Access Forbidden
+        # this status code is soon to be changed to 401 Access Forbidden so handle both of them the same way
