049b11093f1e35fe5374b363f5d2ee2d2ab3b1ce
ItsNeil <neil@willofsteel.me>
2024-04-28 20:10:00 +0530

    remove unwanted code

--- models/units.py
+++ models/units.py
@@ -1,8 +1,7 @@
 from enum import Enum
-from typing import NamedTuple, Callable
+from typing import NamedTuple
 
 import emote
-
 
 class UnitProperties(NamedTuple):
     name: str
@@ -19,24 +18,3 @@ class UnitType(UnitProperties, Enum):
     BIG_BOWMEN = UnitProperties("Big Bowmen", emote.BIG_BOWMEN, 15, 55)
     HEAVY_MEN = UnitProperties("Heavy Men", emote.HEAVY_MEN, 15, 85)
     KINGS_GUARDS = UnitProperties("King's Guards", emote.KINGS_GUARDS, 30, 155)
-
-
-def total_attack(units: dict[UnitType, int]):
-    return sum(map(lambda unit_type: unit_type.attack * units[unit_type], units.keys()))
-
-
-def total_defense(units: dict[UnitType, int]):
-    return sum(map(lambda unit_type: unit_type.defense * units[unit_type], units.keys()))
-
-
-def lost_units(power: int, units: dict[UnitType, int], unit_power: Callable[[UnitType], int]) -> dict[UnitType, int]:
-    lost = {}
-    remaining_power = power
-    sorted_units = sorted(units.items(), key=lambda x: (unit_power(x[0]), list(UnitType).index(x[0])))
-
-    for unit_type, unit_count in sorted_units:
-        units_taken = min(unit_count, round(remaining_power / unit_power(unit_type)))
-        remaining_power -= units_taken * unit_power(unit_type)
-        lost[unit_type] = units_taken
-
-    return lost
