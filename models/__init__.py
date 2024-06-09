7d328416e21c90b392a52cf593f5f90dd5adad57
ItsNeil17 <git@itsneil.tech>
2024-06-09 23:39:48 +0530

    add ruff noqa to init files

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+# ruff: noqa
+from .alliance import Alliance
+from .player import Player, EventPlayer
+from .units import UnitType
+from .characteristics import Characteristics
+from .order import Order
