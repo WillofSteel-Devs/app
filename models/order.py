deff68eef52363487e45f96fdc9465a11a5a3656
ItsNeil17 <git@itsneil.tech>
2024-06-09 23:54:14 +0530

    feat: Remove unused fields from MarketOrder model

--- models/order.py
+++ models/order.py
@@ -4,24 +4,17 @@ __all__ = ("MarketOrder",)
 
 
 class MarketOrder(NamedTuple):
-    uuid: str
-    user_id: int
-    item_id: str
     order_type: str
     price: int
     amount: int
-    fulfilled: int
 
     @staticmethod
     def from_database(row):
         if row is None:
             return None
         return MarketOrder(
-            uuid=row[0],
-            user_id=row[1],
             item_id=row[2],
             order_type=row[3],
             price=row[4],
             amount=row[5],
-            fulfilled=row[6],
         )
