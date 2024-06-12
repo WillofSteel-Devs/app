dedcbf71af4bbfa03db06ba5ec51ae457615a53d
ItsNeil17 <git@itsneil.tech>
2024-06-13 00:14:48 +0530

    refactor: Rename `Order` to `MarketOrder` in models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,4 +3,4 @@ from .alliance import Alliance
 from .player import Player, EventPlayer
 from .units import UnitType
 from .characteristics import Characteristics
-from .order import Order
+from .order import MarketOrder
