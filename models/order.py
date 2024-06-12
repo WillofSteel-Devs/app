6fec339c0f5a2b4257e91735e15fa763b872e399
ItsNeil17 <git@itsneil.tech>
2024-06-13 00:14:54 +0530

    refactor: Update MarketOrder.from_data method to use dictionary input

--- models/order.py
+++ models/order.py
@@ -4,17 +4,18 @@ __all__ = ("MarketOrder",)
 
 
 class MarketOrder(NamedTuple):
+    item_id: str
     order_type: str
     price: int
     amount: int
 
     @staticmethod
-    def from_database(row):
-        if row is None:
+    def from_data(data: dict | None):
+        if data is None:
             return None
         return MarketOrder(
-            item_id=row[2],
-            order_type=row[3],
-            price=row[4],
-            amount=row[5],
+            item_id=data["item_id"],
+            order_type=data["order_type"],
+            price=data["price"],
+            amount=data["amount"],
         )
