3ec83b9dd08e83225699d1c064518a221e3c720a
ItsNeil17 <git@itsneil.tech>
2024-06-13 00:12:08 +0530

    refactor: Rename `path` attribute to `url` in Route class

--- backend/route.py
+++ backend/route.py
@@ -5,17 +5,18 @@ __all__ = ("Route",)
 
 class Route:
     def __init__(self, path, method):
-        self.path = API_URL + path
+        self.url = API_URL + path
+        self.path = path
         self.method = method.upper()
 
     def __str__(self):
-        return self.path
+        return self.url
 
     def __repr__(self):
         return f"Route({self.method} {self.path})"
 
     def __eq__(self, other):
-        return self.path == other.path and self.method == other.method
+        return self.url == other.url and self.method == other.method
 
     def __hash__(self):
-        return hash((self.path, self.method))
+        return hash((self.url, self.method))
