b59757a363983e2659958bea3e9aeae4d38830a5
CozmoKitten <ogriffin2640+altgithub@gmail.com>
2024-06-15 12:27:55 -0400

    Update and rename api_key_submit.py to api_key.py
    add submit button
    
    Signed-off-by: CozmoKitten <ogriffin2640+altgithub@gmail.com>

--- gui/frames/api_key.py
+++ gui/frames/api_key.py
@@ -1,5 +1,5 @@
 import tkinter
-from ..components import labels, inputs
+from ..components import labels, inputs, buttons
 
 
 class APIKeyFrame(tkinter.Frame):
@@ -11,6 +11,7 @@ class APIKeyFrame(tkinter.Frame):
 
         self.apikeyentrylabel = labels.InputLabel(self, "Enter API Key:")
         self.apikeyentry = inputs.TextInput(self)
+        self.apikeysubmit = buttons.SubmitButton(self, text="Submit", width=10, hight=1)
 
     def render(self):
         self.label.grid(row=0, column=0)
@@ -18,3 +19,4 @@ class APIKeyFrame(tkinter.Frame):
         # API key feild stuff
         self.apikeyentrylabel.place(x=300, y=100)
         self.apikeyentry.place(x=190, y=125)
+        self.apikeysubmit.place(x=300, y=150)
