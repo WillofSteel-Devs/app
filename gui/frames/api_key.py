d444119f452e3975bb2fd94590126bc335bda1c4
ItsNeil17 <git@itsneil.tech>
2024-06-15 23:22:39 +0530

    add api key checks

--- gui/frames/api_key.py
+++ gui/frames/api_key.py
@@ -1,5 +1,6 @@
 import tkinter
 from ..components import labels, inputs, buttons
+from backend.api import API
 
 
 class APIKeyFrame(tkinter.Frame):
@@ -11,7 +12,29 @@ class APIKeyFrame(tkinter.Frame):
 
         self.apikeyentrylabel = labels.InputLabel(self, "Enter API Key:")
         self.apikeyentry = inputs.TextInput(self)
-        self.apikeysubmit = buttons.SubmitButton(self, text="Submit", width=10, hight=1)
+        self.apikeysubmit = buttons.SubmitButton(
+            self, text="Submit", width=10, hight=1, command=self.submit_api_key
+        )
+        self.show_error("test")
+
+    def submit_api_key(self):
+        api_key = self.apikeyentry.get()
+        backend = API(api_key)
+        valid = backend.verify_key()
+        if valid:
+            with open("API_KEY.txt", "w") as f:
+                f.write(self.apikeyentry.get())
+            self.parent.backend = backend
+            self.parent.build_main_frame()
+        else:
+            self.apikeyentry.delete(0, "end")
+            self.apikeyentry.insert(0, "Invalid API Key")
+
+    def show_error(self, error: str):
+        label = labels.PopUpLabel(
+            self, error, bg="red", fg="white", relief=tkinter.SUNKEN
+        )
+        label.place(x=200, y=50, anchor="center")
 
     def render(self):
         self.label.grid(row=0, column=0)
