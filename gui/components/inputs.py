63df5e92e75f657fce5ee439efd164196e533743
CozmoKitten <ogriffin2640+altgithub@gmail.com>
2024-06-15 13:01:44 -0400

    add dropdown component type
    Signed-off-by: CozmoKitten <ogriffin2640+altgithub@gmail.com>

--- gui/components/inputs.py
+++ gui/components/inputs.py
@@ -9,3 +9,19 @@ class TextInput(tkinter.Entry):
             *args,
             **kwargs,
         )
+        
+class Dropdown(tkinter.OptionMenu):
+    def __init__(self, parent, options=[], default=None, *args, **kwargs):
+        self.selected_value = tkinter.StringVar(parent)
+        
+        if default:
+            self.selected_value.set(default)
+        else:
+            self.selected_value.set(options[0] if options else '')
+        
+        super().__init__(
+            parent, 
+            self.selected_value, 
+            *options, 
+            *args, 
+            **kwargs)
