7a375a11e2bca184f5aef624b22e979dc424e832
CozmoKitten <ogriffin2640+altgithub@gmail.com>
2024-06-15 21:05:27 -0400

    Update inputs.py
    Signed-off-by: CozmoKitten <ogriffin2640+altgithub@gmail.com>

--- gui/components/inputs.py
+++ gui/components/inputs.py
@@ -9,3 +9,32 @@ class TextInput(tkinter.Entry):
             *args,
             **kwargs,
         )
+
+
+class IntergerOnlyEntry(tkinter.Entry):
+    def __init__(self, parent, minNumber=None, maxNumber=None, *args, **kwargs):
+        self.minNumber = minNumber
+        self.maxNumber = maxNumber
+
+        self.var = tkinter.StringVar()
+
+        super().__init__(parent, textvariable=self.var, *args, **kwargs)
+
+        self.validate_command = (self.register(self.validate_input), "%P")
+
+        self.config(validate="key", validatecommand=self.validate_command)
+
+    def validate_input(self, new_value):
+        if new_value.isdigit():
+            check_value = int(new_value)
+            # check if number within range, if range specified
+            if self.maxNumber is not None:
+                if not (check_value > self.maxNumber):
+                    self.var = self.maxNumber  # force value to max
+            if self.minNumber is not None:
+                if not (check_value < self.minNumber):
+                    self.var = self.minNumber  # force value to min
+
+            return True
+
+        return False
