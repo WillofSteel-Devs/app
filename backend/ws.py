750330164b27b6825f9fe953f04636f786c02109
ItsNeil17 <git@itsneil.tech>
2024-06-28 15:51:01 +0530

    use websockets instead of socket

--- backend/ws.py
+++ backend/ws.py
@@ -1,17 +1,16 @@
 import json
-import socket
 import threading
+from websockets.sync import client
 
 from . import events
 
 class Websocket:
     def __init__(self, uri) -> None:
         self.uri = uri
-        self._ws = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.listeners = {}
 
     def send(self, message: str):
-        self._ws.send(message.encode())
+        self._ws.send(json.dumps(message))
 
     def on(self, event: str, callback):
         if not self.listeners.get(event):
@@ -31,29 +30,11 @@ class Websocket:
         func = self.listeners.get(event)
         if func: func(args)
 
-    def listen(self):
-        while True:
-            try:
-                message = self._ws.recv(1024).decode()
-                print(message)
-                if message: self.handle_message(message)
-            except Exception as e:
-                print(e)
-                break
-
     def connect(self):
-        self._ws.connect(self.get_host())
-        thread = threading.Thread(target=self.listen)
-        thread.start()
-        print(f"connected to {self.get_host()}")
-
-    def get_host(self):
-        host =  self.uri.split("//")[1].split("/")[0]
-        if ":" in host:
-            host, port = host.split(":")
-            return (host, int(port))
-        else:
-            return host
+        with client.connect(self.uri) as ws:
+            self._ws = ws
+            for message in ws:
+                self.handle_message(message)
 
     # Helper functions
     def get_alliance(self, callback):
